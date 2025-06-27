7cb5ce72737687ae8f68c83b5ba134fd8e0ddd0e
Zufan-Hadgu <zufanhadgu1@gmail.com>
2025-06-27 16:56:36 +0300

    corrected image alignment

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,24 @@ function App() {
       <Lights />
       <GalleryRoom />
 
-  
+      {/* === Back Wall Art (front-facing) === */}
       <ArtworkFrame textureUrl="/assets/images/art1.jpg" position={[-6, 2.5, -14.95]} />
       <ArtworkFrame textureUrl="/assets/images/art2.jpg" position={[0, 2.5, -14.95]} />
       <ArtworkFrame textureUrl="/assets/images/art3.jpg" position={[6, 2.5, -14.95]} />
-
+{/* === Left Wall Art (side-view flush with wall) === */}
 <ArtworkFrame textureUrl="/assets/images/art4.jpg" position={[-11.95, 2.5, -6]} rotation={[0, Math.PI / 2, 0]} />
 <ArtworkFrame textureUrl="/assets/images/art5.jpg" position={[-11.95, 2.5, 0]} rotation={[0, Math.PI / 2, 0]} />
 <ArtworkFrame textureUrl="/assets/images/art6.jpg" position={[-11.95, 2.5, 6]} rotation={[0, Math.PI / 2, 0]} />
 
+{/* === Right Wall Art (side-view flush with wall) === */}
 <ArtworkFrame textureUrl="/assets/images/art5.jpg" position={[11.95, 2.5, -6]} rotation={[0, -Math.PI / 2, 0]} />
 <ArtworkFrame textureUrl="/assets/images/art4.jpg" position={[11.95, 2.5, 0]} rotation={[0, -Math.PI / 2, 0]} />
 <ArtworkFrame textureUrl="/assets/images/art6.jpg" position={[11.95, 2.5, 6]} rotation={[0, -Math.PI / 2, 0]} />
 
-      <ArtworkFrame textureUrl="/assets/images/art1.jpg" position={[0.75, 2.5, -5]} rotation={[0, -Math.PI / 2, 0]} />
-      <ArtworkFrame textureUrl="/assets/images/art2.jpg" position={[-0.75, 2.5, 5]} rotation={[0, Math.PI / 2, 0]} />
+
+      {/* === Section Divider Art (grounded) === */}
+      <ArtworkFrame textureUrl="/assets/images/art1.jpg" position={[0.75, 1.5, -5]} rotation={[0, -Math.PI / 2, 0]} />
+      <ArtworkFrame textureUrl="/assets/images/art2.jpg" position={[-0.75, 1.5, 5]} rotation={[0, Math.PI / 2, 0]} />
 
       <OrbitControls />
     </Canvas>
--- src/components/ArtWorkFrame.jsx
+++ src/components/ArtWorkFrame.jsx
@@ -1,14 +1,15 @@
 import { useRef } from 'react';
 import { useTexture } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
 
-export default function ArtworkFrame({ textureUrl, position }) {
+export default function ArtworkFrame({ textureUrl, position, rotation }) {
   const ref = useRef();
   const texture = useTexture(textureUrl);
 
   return (
-    <mesh ref={ref} position={position}>
-      <boxGeometry args={[2, 2.5, 0.1]} />
-      <meshStandardMaterial map={texture} />
+    <mesh ref={ref} position={position} rotation={rotation}>
+      <planeGeometry args={[2, 2.5]} />
+      <meshStandardMaterial map={texture} side={2} /> {/* side=2 = DoubleSide */}
     </mesh>
   );
 }
