1e223396e6c3f71b9fcfc029d8aa61e1c9d90090
Zufan-Hadgu <zufanhadgu1@gmail.com>
2025-06-28 22:12:46 +0300

    edited lighting

--- lighting.js
+++ lighting.js
@@ -1,11 +1,18 @@
 import * as THREE from 'https://esm.sh/three@0.160.1';
 
 export function setupLighting(scene) {
-  const ambient = new THREE.AmbientLight(0xffffff, 0.4);
-  const dirLight = new THREE.DirectionalLight(0xffffff, 1);
-  dirLight.position.set(10, 10, 10);
-  dirLight.castShadow = true;
 
+  const ambient = new THREE.AmbientLight(0xffffff, 0.5);
   scene.add(ambient);
-  scene.add(dirLight);
+
+  const dirLight1 = new THREE.DirectionalLight(0xffffff, 0.7);
+  dirLight1.position.set(10, 10, 10);
+  dirLight1.castShadow = true;
+  scene.add(dirLight1);
+
+  
+  const dirLight2 = new THREE.DirectionalLight(0xffffff, 0.7);
+  dirLight2.position.set(-10, 10, -10);
+  dirLight2.castShadow = false; 
+  scene.add(dirLight2);
 }
