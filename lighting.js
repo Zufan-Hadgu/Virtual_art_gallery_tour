dc6a9e55a5cd4a7da9eb23a6cbb059a533aec135
Tsega Ephrem <tsegaephrem080@gmail.com>
2025-06-29 22:55:27 -0400

    modified lighting

--- lighting.js
+++ lighting.js
@@ -10,7 +10,6 @@ export function setupLighting(scene) {
   dirLight1.castShadow = true;
   scene.add(dirLight1);
 
-  
   const dirLight2 = new THREE.DirectionalLight(0xffffff, 0.7);
   dirLight2.position.set(-10, 10, -10);
   dirLight2.castShadow = false; 
